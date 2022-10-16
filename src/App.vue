75926d8a031ae8e495f8f04cc29e1746568566a1
Armand Ribault <ribaultarmand@gmail.com>
2022-10-16 10:26:05 +0200

    center header

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,16 @@ import { RouterLink, RouterView } from "vue-router";
 <template>
   <header>
     <img
-      alt="Vue logo"
+      alt="Profitroles logo"
       class="logo"
       src="@/assets/logo.svg"
       width="125"
       height="125"
     />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">L'association</RouterLink>
-        <RouterLink to="/events">Nos représentations</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/">L'association</RouterLink>
+      <RouterLink to="/events">Nos représentations</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
@@ -27,18 +24,18 @@ import { RouterLink, RouterView } from "vue-router";
 header {
   line-height: 1.5;
   max-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
+  font-size: 24px;
   text-align: center;
-  margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
@@ -55,29 +52,8 @@ nav a {
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
   .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    margin-right: 1em;
   }
 }
 </style>
