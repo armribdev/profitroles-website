da30f7676f160d4a18c2d9d7e54280bba54211e6
Armand Ribault <ribaultarmand@gmail.com>
2022-10-22 12:36:15 +0200

    add event-card

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="events">
     <h1>Vous êtes sur la page des évènements</h1>
+    <div class="event-card">
+      <img src="@/assets/logo.svg"/>
+      <BaseDateLabel dateTime="2022-11-26 14:45:00"/>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
+<script>
+import BaseDateLabel from '@/components/widgets/BaseDateLabel.vue'
 
+export default {
+  components: {
+    BaseDateLabel,
+  },
 }
+</script>
+
+<style>
 </style>
