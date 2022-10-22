eb715344235ffc2bdeaedb2f6ee178b4c7d97609
Armand Ribault <ribaultarmand@gmail.com>
2022-10-22 12:33:48 +0200

    add BaseDateLabel.vue

---
+++ src/components/widgets/BaseDateLabel.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="date">
+        <span class="day-of-the-week">{{ day }}</span>
+        <span class="wrapper">
+          <span class="day">{{ date }}</span>
+          <span class="mounth">{{ month }}</span>
+        </span>
+    </div>
+  </template>
+  
+<script>
+const months = ['jan', 'fév', 'mar', 'avr', 'mai', 'jun', 'jul', 'aoû', 'sep', 'oct', 'nov', 'dec']
+const days = ['dim', 'lun', 'mar', 'mer', 'jeu', 'ven', 'sam']
+
+export default {
+  props: {
+    dateTime: String,
+  },
+  computed: {
+    date() {
+      const date = new Date(this.dateTime)
+      return date.getDate()
+    },
+    day() {
+      const date = new Date(this.dateTime)
+      return days[date.getDay()].toUpperCase()
+    },
+    month() {
+      const date = new Date(this.dateTime)
+      return months[date.getMonth()].toUpperCase()
+    },
+  }
+}
+</script>
+
+<style>
+.date {
+  font-family: "Space Mono", monospace;
+}
+
+.day-of-the-week {
+  display: block;
+  writing-mode: tb-rl
+}
+
+.wrapper {
+  display: block;
+}
+</style>
