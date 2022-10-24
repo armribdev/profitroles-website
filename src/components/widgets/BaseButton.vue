56506ee56f1412c76aed5f0b1735719edde6dabd
Armand Ribault <ribaultarmand@gmail.com>
2022-10-24 12:41:33 +0200

    add BaseButton

---
+++ src/components/widgets/BaseButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button>{{ label }}</button>
+</template>
+  
+<script>
+export default {
+  props: {
+    label: String,
+  },
+}
+</script>
+
+<style scoped>
+button {
+  border-radius: .4rem;
+  font-size: 1rem;
+  padding: .5rem 1rem;
+  background: var(--profitroles-color);
+  color: white;
+  border: none;
+  font-weight: 600;
+}
+
+button:hover {
+  background: #bb0000;
+}
+</style>
