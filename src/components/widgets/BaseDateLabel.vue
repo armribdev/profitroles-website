9634f3932ae3b9f0bb8f10bdc578bb0891a04d17
Armand Ribault <ribaultarmand@gmail.com>
2022-10-24 18:18:00 +0200

    improve css

--- src/components/widgets/BaseDateLabel.vue
+++ src/components/widgets/BaseDateLabel.vue
@@ -2,14 +2,14 @@
     <span class="dateTimeLabel">
       <span class="dateLabel">
         <span class="day">{{ day }}</span>
-        <span class="right">
+        <span class="date-right">
           <span class="date">{{ date }}</span>
           <span class="month">{{ month }}</span>
         </span>
       </span>
       <span class="timeLabel">
         <span class="hours">{{ hours }}</span>
-        <span class="bottom">
+        <span class="time-right">
           <span class="timeSeparator">H</span>
           <span class="minutes">{{  minutes }}</span>
         </span>
@@ -58,7 +58,7 @@ export default {
 }
 
 .dateLabel {
-  font-family: "Space Mono", monospace;
+  font-family: "Source Code Pro", "Space Mono", monospace;
   display: flex;
   align-items: center;
   line-height: 1em;
@@ -69,17 +69,20 @@ export default {
   writing-mode: tb-rl;
   font-size: 1.3em;
   color: lightgray;
+  font-weight: 600;
 }
 
 .month {
   font-size: 1.2em;
+  font-weight: 700;
 }
 
 .date {
   font-size: 2em;
+  font-weight: 700;
 }
 
-.right {
+.date-right {
   display: flex;
   flex-direction: column;
   line-height: 1.4em;
@@ -88,17 +91,36 @@ export default {
 
 .timeLabel {
   margin-left: 1em;
+  font-weight: 700;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 
 .hours {
   font-size: 2em;
+  font-weight: 700;
 }
 
 .bottom {
   font-size: 1.2em;
 }
 
+.time-right {
+  display: flex;
+  flex-direction: column;
+  line-height: .8em;
+  margin-left: .1em;
+}
+
 .timeSeparator {
   color: lightgray;
+  font-weight: 700;
+  font-size: .7em;
+}
+
+.minutes {
+  font-weight: 700;
+  font-size: 1.2em;
 }
 </style>
