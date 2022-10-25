b068c3f0e90d682afa6a61387cabd220831d67c1
Armand Ribault <ribaultarmand@gmail.com>
2022-10-25 22:06:06 +0200

    add EventCard.vue

---
+++ src/components/EventCardList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="event-list">
+      <EventCard
+        v-for="(event, index) in events"
+        :name="event.name"
+        :type="event.type"
+        :posterFilename="event.posterFilename"
+        :dateTime="event.dateTime"
+        :ticketingURL="event.ticketingURL"
+        :ticketingOpen="event.ticketingOpen"
+        :key="index"
+       />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard,
+  },
+  props: {
+    events: {
+      type: Array,
+      required: false,
+      default: () => [],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.event-list {
+  text-align: left;
+  margin-top: 4em;
+}
+</style>
