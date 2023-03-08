7877034cb82aa9d7828e760f92f5af3060edc927
Armand Ribault <ribaultarmand@gmail.com>
2023-03-08 14:30:53 +0100

    add credits

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,28 @@ import { RouterLink, RouterView } from "vue-router";
 <template>
   <header>
     <img alt="Profitroles logo" class="logo" src="@/assets/logo.svg" />
-    <nav>
+    <nav class="website-nav">
       <RouterLink to="/">Découvrir</RouterLink>
       <RouterLink to="/events">Nos représentations</RouterLink>
     </nav>
   </header>
   <RouterView class="router-view" />
   <footer class="footer">
-    <a href="https://www.facebook.com/leo.profitroles.9">Facebook</a>
-    <a href="https://instagram.com/profit.roles?igshid=YmMyMTA2M2Y="
-      >Instagram</a
-    >
-    <a href="mailto:profitroles.utc@gmail.com">Nous écrire</a>
+    <nav class="external-links">
+      <a href="https://www.facebook.com/leo.profitroles.9">Facebook</a>
+      <a href="https://instagram.com/profit.roles?igshid=YmMyMTA2M2Y="
+        >Instagram</a
+      >
+      <a href="mailto:profitroles.utc@gmail.com">Nous écrire</a>
+    </nav>
+    <div class="credits">
+      <span>Crédits</span>
+      <span>Illustrations : Julie Asselin</span>
+      <span
+        >Site : Armand Ribault
+        <!-- ce bg enfaite --></span
+      >
+    </div>
   </footer>
 </template>
 
@@ -34,23 +44,23 @@ header {
   width: 140px;
 }
 
-nav {
+.website-nav {
   font-size: 30px;
   text-align: center;
 }
 
-nav a.router-link-exact-active {
+.website-nav a.router-link-exact-active {
   color: var(--color-text);
 }
 
-nav a {
+.website-nav a {
   display: inline-block;
   padding: 0 1rem;
   font-weight: 500;
   line-height: 1em;
 }
 
-.footer {
+.external-links {
   display: flex;
   justify-content: center;
   gap: 1em;
@@ -60,12 +70,22 @@ nav a {
   height: 3em;
 }
 
-.footer a {
+.external-links a {
   opacity: 0.8;
 }
 
-.footer a:hover {
+.external-links a:hover {
   opacity: 1;
+}
+
+.credits {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 0.8em;
+  opacity: 0.8;
 }
 
 @media (max-width: 534px) {
@@ -85,13 +105,13 @@ nav a {
 }
 
 @media (hover: hover) and (min-width: 534px) {
-  nav a:hover {
+  .website-nav a:hover {
     transform: translateY(-0.2em);
   }
 }
 
 @media (hover: hover) {
-  nav a:hover {
+  .website-nav a:hover {
     color: rgb(231, 185, 185);
   }
 }
