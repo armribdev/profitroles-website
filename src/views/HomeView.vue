21076f14e9dd8adb002c2c128ad4eacaec521ead
Armand Ribault <ribaultarmand@gmail.com>
2023-03-08 13:49:58 +0100

    add descriptions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,122 +1,123 @@
 <template>
-  <div class="events">
-    <section class="association">
-      <h1 class="association-title">L'association</h1>
-      <div class="wrapper">
-        <img class="association-image" src="@/assets/logo.svg" />
-        <p class="association-description">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa impedit quos asperiores earum voluptatem nulla sit laboriosam maxime velit consequuntur quam quis, blanditiis, enim optio dignissimos cumque architecto. Autem, asperiores!
-        </p>
-      </div>
+  <div class="home">
+    <section id="association" class="workshop">
+      <img class="workshop-illustration" src="@/assets/illustrations/association.png" />
+      <p class="workshop-description">
+        Profit'Rôles est l'asso de théâtre de l'UTC&#xA0;!
+        Fondée en 2006, elle s'est largement développée depuis et vous propose aujourd'hui pas moins de 5 ateliers
+        pendant le semestre...
+      </p>
     </section>
-    <section class="workshop">
-      <h2 class="workshop-title">Pièce</h2>
-      <div class="wrapper">
-        <img class="workshop-image" src="@/assets/logo.svg" />
-        <p class="workshop-description">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut facere quia dolores facilis fugiat perferendis obcaecati beatae incidunt quae quod ipsam, dolorum non quas rem sunt sapiente earum corrupti.
-        </p>
-      </div>
+    <section id="piece" class="workshop">
+      <h2 class="workshop-title">Pièce de théâtre</h2>
+      <img class="workshop-illustration" src="@/assets/illustrations/piece.png" />
+      <p class="workshop-description">
+        La pièce est l’événement incontournable qui a traversé les âges et les confinements. Nous nous réunissons deux fois par semaine pour répéter et préparer un spectacle. Celui-ci va égayer les Compiègnois et autres UTCéens en stress pré-examens, en fin de semestre. <br />
+        Les registres sont larges, allant du classique molière au drame russe, en passant par des textes contemporains&#xA0;: comédies de boulevard et autres comédies dramatiques. C’est toute une équipe qui se met en marche pour fournir le plus grand des spectacles&#xA0;: acteurs, metteur en scène, décors, costumes, et parfois musique, danse, chant (toute ressemblance avec une autre association serait purement fortuite).<br />
+        Nous jouons nos pièces de théâtre à l’occasion du FESTUPIC, mais pas que, parce qu’il n’y a pas besoin d’une occasion spéciale pour vivre le théâtre&#xA0;! <br />
+        La préparation de la pièce, c’est aussi une aventure humaine, ce sont des rencontres, des moments de grande intensité (qui a dit, 7e UV ?), et beaucoup d’apprentissage. Pour rejoindre cette grande aventure, les castings sont organisés en début de semestre, même pour les débutants. <br />
+        Venez nous voir en début de semestre à la journée des associations (JDA) pour faire le grand saut&#xA0;!<br/>
+        A bientôt sur les planches.
+      </p>
     </section>
-    <section class="workshop">
+    <section id="drama" class="workshop">
       <h2 class="workshop-title">Atelier drame</h2>
-      <div class="wrapper">
-        <img class="workshop-image" src="@/assets/logo.svg" />
-        <p class="workshop-description">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut facere quia dolores facilis fugiat perferendis obcaecati beatae incidunt quae quod ipsam, dolorum non quas rem sunt sapiente earum corrupti.
-        </p>
-      </div>
+      <img class="workshop-illustration" src="@/assets/illustrations/drama.png" />
+      <p class="workshop-description">
+        Dans l’atelier drame, nous travaillons nos émotions en toute sincérité et en toute intimité.
+        Nous suivons les exercices classiques d’acteurs pour placer sa voix, son corps, ses émotions.
+        Nous jouons des scènes entre nous pour le plaisir de vivre, de partager, de progresser.
+        La bienveillance est au rendez-vous. <br />
+        Que vous souhaitiez découvrir le théâtre, ou alors approfondir votre jeu, n’hésitez pas&#xA0;: cet atelier sera parfait pour vous&#xA0;!
+        Vous pouvez joindre une séance à tout moment dans le semestre.
+      </p>
     </section>
-    <section class="workshop">
+    <section id="impro" class="workshop">
       <h2 class="workshop-title">Improvisation</h2>
-      <div class="wrapper">
-        <img class="workshop-image" src="@/assets/logo.svg" />
-        <p class="workshop-description">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut facere quia dolores facilis fugiat perferendis obcaecati beatae incidunt quae quod ipsam, dolorum non quas rem sunt sapiente earum corrupti.
-        </p>
-      </div>
+      <img class="workshop-illustration" src="@/assets/illustrations/impro.png" />
+      <p class="workshop-description">
+        <blockquote class="citation">
+          “La vache est tombée dans la piscine&#xA0;! <br />
+          - Ah, c’est pas très hygiénique. <br />
+          - Oui, elle va avoir du chlore partout&#xA0;!” <br />
+        </blockquote>
+        A l’impro courte, nous disons parfois des choses intelligentes, comme “La tomate est à la fois un fruit et un légume”.
+        Nous disons parfois des choses absurdes, comme “Vous savez quoi ? On fait un concours. Celui qui gagne, il a gagné”.
+        Oui bien encore, des choses essentielles, comme “Nous n’aimons pas la démocratie, nous pensons qu’elle sclérose la société&#xA0;!”.
+        A partir d’un simple thème, d’un simple lieu, la situation peut vite s’emballer.
+        Il faut croire que c’est plus intéressant que la vraie vie&#xA0;! <br />
+        L’important, c’est qu’à l’impro courte, on s’amuse beaucoup.
+        Nous nous entraînons en séance, et pour ceux qui le sentent, nous présentons nos talents durant nos spectacles au cours du semestre.
+        Nous jouons aussi en partenariat avec d’autres associations (Festupic, IF, Utcéenne…).
+        Notre mode de prédilection est le cabaret d’impro (aka la Profit’Soirée).
+        Mais nous avons également le match d’impro comme corde à notre arc&#xA0;! <br />
+        Pour gagner en confiance, en répondant, mais surtout en mimes et en bruitages, c’est par ici&#xA0;!
+        Vous pouvez joindre une séance à tout moment dans le semestre.
+      </p>
     </section>
-    <section class="workshop">
-      <h2 class="workshop-title">Impro longue</h2>
-      <div class="wrapper">
-        <img class="workshop-image" src="@/assets/logo.svg" />
-        <p class="workshop-description">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut facere quia dolores facilis fugiat perferendis obcaecati beatae incidunt quae quod ipsam, dolorum non quas rem sunt sapiente earum corrupti.
-        </p>
-      </div>
+    <section id="longform" class="workshop">
+      <h2 class="workshop-title">Impro longform</h2>
+      <img class="workshop-illustration" src="@/assets/illustrations/impro-longue.png" />
+      <p class="workshop-description">
+        Comment dépenser 45 minutes de son temps ? Certains regardent un épisode d’une série à succès.
+        Notre truc, c’est de créer et jouer notre propre histoire complètement improvisée.
+        Alors, “improvisée” ne veut pas dire qu’on arrive les mains dans les poches.
+        L’impro longue, c’est un atelier pour apprendre à créer des histoires, des personnages.
+        C’est un atelier pour ceux qui aiment beaucoup l’improvisation et qui veulent aller toujours plus loin et progresser.
+        Ou bien pour ceux qui voudraient jouer une pièce mais n’auraient pas la patience d’apprendre un texte.
+        L’impro longue c’est tout ça et bien plus encore. <br />
+        Venez nous voir en début de semestre à la journée des associations (JDA) pour faire le grand saut&#xA0;!
+      </p>
     </section>
-    <section class="workshop">
-      <h2 class="workshop-title">Ouvertures au Théâtre à Moustaches</h2>
-      <div class="wrapper">
-        <img class="workshop-image" src="@/assets/logo.svg" />
-        <p class="workshop-description">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut facere quia dolores facilis fugiat perferendis obcaecati beatae incidunt quae quod ipsam, dolorum non quas rem sunt sapiente earum corrupti.
-        </p>
-      </div>
+    <section id="opening" class="workshop">
+      <h2 class="workshop-title">Ouvertures au TAM</h2>
+      <!-- <img class="workshop-illustration" src="@/assets/logo.svg" /> -->
+      <p class="workshop-description">
+        A Profit’Rôles, on aime tellement le théâtre, qu’en plus d’en faire, on en voit.
+        Et puis, comment progresser sans voir les meilleurs à l'œuvre&#xA0;?
+        Grâce à un partenariat avec le Théâtre à Moustaches, vous pouvez aller voir une comédie gratuitement, en échange d’une ouverture&#xA0;!
+        Vous choisissez la pièce et le jour, vous arrivez plus tôt, vous laissez entrer les gens, puis vous vous asseyez et vous profitez.
+        La programmation est disponible ici.<br />
+        Pour faire une ouverture, rendez-vous le jeudi à 19h15, les vendredi et samedi à 20h15.
+        Contactez-nous pour vous inscrire à la date de votre choix&#xA0;!
+      </p>
     </section>
-    <section class="workshop">
+    <section id="other" class="workshop">
       <h2 class="workshop-title">Divers</h2>
-      <div class="wrapper">
-        <p class="workshop-description">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut facere quia dolores facilis fugiat perferendis obcaecati beatae incidunt quae quod ipsam, dolorum non quas rem sunt sapiente earum corrupti.
-        </p>
-      </div>
+      <p class="workshop-description">
+        Profit’Rôles est un vivier d’UTCéens passionnés de jeu : vous pouvez nous solliciter pour vos courts-métrages,
+        vos animations d’événement, vos mariages et anniversaires (bon d’accord, peut-être pas) <br />
+        A bientôt sur les planches&#xA0;!
+      </p>
     </section>  
   </div>
 </template>
 
 <style scoped>
-h1, h2 {
-  text-align: center;
+.home {
+  margin-bottom: 1em;
+  padding: 1rem;
 }
-
-
-section .wrapper{
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-section .wrapper img {
-  max-height: 600px;
-  max-width: 600px;
-  margin: auto;
-}
-
 .workshop {
   margin-top: 3em;
 }
-
-.workshop-description {
-  margin-top: 1.5em;
+.workshop-title {
+  text-align: center;
+  line-height: 1.2em;
+  font-size: 2.5em;
+  font-weight: 600;
+}
+.workshop-illustration {
+  max-height: 600px;
+  max-width: min(600px, 100%);
+  display: block;
+  margin: 1em auto;
 }
 
-
-
-@media (min-width: 1024px) {
-  .workshop-description {
-    margin-top: 0;
-  }
-
-  .workshop:nth-child(2n) .workshop-description {
-    margin-left: 1.5em;
-  }
-
-  .workshop:nth-child(2n) .wrapper {
-    flex-direction: row;
-  }
-
-  .workshop:nth-child(2n+1) .workshop-description {
-    margin-right: 1.5em;
-  }
-
-  .workshop:nth-child(2n+1) .wrapper {
-    flex-direction: row-reverse;
-  }
-
-  .workshop:last-child .workshop-description {
-    margin: 0;
-  }
-
+.citation {
+  text-align: center;
+  font-size: 1.1em;
+  margin-bottom: .5em;
+  font-weight: 500;
 }
 </style>
